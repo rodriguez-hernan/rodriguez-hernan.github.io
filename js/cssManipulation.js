3c12a0b5463ca264127fce46ca066d7412d85c6d
hernan-rodriguez <itwarp_hrodriguez@iseatz.com>
2019-10-30 02:42:04 -0300

    add javascript manipulating css, and transitions animations page

---
+++ js/cssManipulation.js
@@ -0,0 +1,36 @@
+function changeFont(event) {
+  document.getElementById("scripture").style.fontSize = event.value + 'px';
+}
+
+function changePadding(event) {
+  document.getElementById("scripture").style.padding = event.value + 'px';
+}
+
+function changeWS(e) {
+  document.getElementById("scripture").style.wordSpacing = e.value + 'px';
+}
+
+function changeBackColor(e) {
+  document.getElementById("scripture").style.backgroundColor = e.value;
+}
+
+function changeColor(e) {
+  document.getElementById("scripture").style.color = e.value;
+}
+
+function changeFontFamily(e) {
+  document.getElementById("scripture").style.fontFamily = e.value;
+}
+
+function setAnimation1(e) {
+  console.log(event)
+}
+
+function setAnimation2(e) {
+  console.log(event)
+}
+
+function setAnimation3(e) {
+  console.log(event)
+}
+
