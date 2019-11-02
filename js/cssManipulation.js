59be3d37af013c21c3a81dda750ec9578a17c644
Henry <hobbit.rodrigiu@gmail.com>
2019-11-02 16:42:12 -0300

    add animations

--- js/cssManipulation.js
+++ js/cssManipulation.js
@@ -1,36 +1,35 @@
 function changeFont(event) {
-  document.getElementById("scripture").style.fontSize = event.value + 'px';
+    document.getElementById("scripture").style.fontSize = event.value + 'px';
 }
 
 function changePadding(event) {
-  document.getElementById("scripture").style.padding = event.value + 'px';
+    document.getElementById("scripture").style.padding = event.value + 'px';
 }
 
 function changeWS(e) {
-  document.getElementById("scripture").style.wordSpacing = e.value + 'px';
+    document.getElementById("scripture").style.wordSpacing = e.value + 'px';
 }
 
 function changeBackColor(e) {
-  document.getElementById("scripture").style.backgroundColor = e.value;
+    document.getElementById("scripture").style.backgroundColor = e.value;
 }
 
 function changeColor(e) {
-  document.getElementById("scripture").style.color = e.value;
+    document.getElementById("scripture").style.color = e.value;
 }
 
 function changeFontFamily(e) {
-  document.getElementById("scripture").style.fontFamily = e.value;
+    document.getElementById("scripture").style.fontFamily = e.value;
 }
 
 function setAnimation1(e) {
-  console.log(event)
+    document.getElementById("scripture").classList.toggle("animation-1");
 }
 
 function setAnimation2(e) {
-  console.log(event)
+    document.getElementById("scripture").classList.toggle("animation-2");
 }
 
 function setAnimation3(e) {
-  console.log(event)
+    document.getElementById("scripture").classList.toggle("animation-3");
 }
-
