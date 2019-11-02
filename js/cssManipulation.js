148d6408bd3bb1d46d99b49d0f6b3be03a3471cc
Henry <hobbit.rodrigiu@gmail.com>
2019-11-02 18:09:28 -0300

    fix animations

--- js/cssManipulation.js
+++ js/cssManipulation.js
@@ -23,13 +23,34 @@ function changeFontFamily(e) {
 }
 
 function setAnimation1(e) {
-    document.getElementById("scripture").classList.toggle("animation-1");
+    var scripture = document.getElementById("scripture");
+    if (e.checked) {
+        document.getElementById("animation2").checked = false;
+        scripture.classList.remove("animation-2");
+        document.getElementById("animation3").checked = false;
+        scripture.classList.remove("animation-3");
+    }
+    scripture.classList.toggle("animation-1");
 }
 
 function setAnimation2(e) {
-    document.getElementById("scripture").classList.toggle("animation-2");
+    var scripture = document.getElementById("scripture");
+    if (e.checked) {
+        document.getElementById("animation1").checked = false;
+        scripture.classList.remove("animation-1");
+        document.getElementById("animation3").checked = false;
+        scripture.classList.remove("animation-3");
+    }
+    scripture.classList.toggle("animation-2");
 }
 
 function setAnimation3(e) {
-    document.getElementById("scripture").classList.toggle("animation-3");
+    var scripture = document.getElementById("scripture");
+    if (e.checked) {
+        document.getElementById("animation1").checked = false;
+        scripture.classList.remove("animation-1");
+        document.getElementById("animation2").checked = false;
+        scripture.classList.remove("animation-2");
+    }
+    scripture.classList.toggle("animation-3");
 }
