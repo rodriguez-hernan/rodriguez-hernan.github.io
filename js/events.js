0c47958f2c75a2d25804b55fca479b3e6bdc1c99
Henry <hobbit.rodrigiu@gmail.com>
2019-11-09 02:18:55 -0300

    add the rest of js events

--- js/events.js
+++ js/events.js
@@ -1,12 +1,10 @@
 (function() {
-    console.log('starting')
     document.getElementById("start-button").addEventListener('click', addStartingContent);
 })();
 
 function addStartingContent() {
-    console.log('clicked')
+    document.querySelector(".title").classList.add('title-contrast');
     addMonkeyImg();
-    // add event
     const monkey = document.getElementById("monkey");
     if (monkey) {
         monkey.addEventListener('mouseenter', () => {
@@ -15,37 +13,86 @@ function addStartingContent() {
         monkey.addEventListener('mouseleave', () => {
             monkey.src = "../assets/monkey-2.png";
         })
+        monkey.addEventListener('mousedown', () => {
+            monkey.src = "../assets/surprised.jpg";
+        })
     }
 
     addMagicInput();
+    const inputBox = document.getElementById("input-box");
+    const wordArr = [];
+    let bigWordTrigger = false;
+
+    if (inputBox) {
+        inputBox.addEventListener('keyup', (event) => {
+            if (event.key.length < 2) {
+                wordArr.push(event.key);
+            }
+            if (wordArr.length > 0 && wordArr.length < 4) {
+                inputBox.classList.add('red-back');
+            } else if (wordArr.length > 3 && wordArr.length < 7) {
+                inputBox.classList.remove('red-back');
+                inputBox.classList.add('yellow-back');
+            } else if (wordArr.length > 6) {
+                console.log('bigWordTrigger', bigWordTrigger);
+                inputBox.classList.remove('yellow-back');
+                inputBox.classList.add('green-back');
+                bigWordTrigger = bigwords(bigWordTrigger);
+                document.getElementById("congrats-img")
+                    .addEventListener("dblclick", () => location = location);
+                console.log('Double click on the last image to start again!')
+            }
+        })
+    }
+
+    document.querySelector("#content").addEventListener('mousemove', (event) => {
+        var x = event.clientX; // Get the horizontal coordinate
+        var y = event.clientY; // Get the vertical coordinate
+        var coor = "X coords: " + x + ", Y coords: " + y;
+        var hexString = (x + y).toString(16);
+        document.getElementById("content").style.backgroundColor = '#' + hexString;
+    })
+
 }
 
 
 function addMonkeyImg() {
-    console.log('adding monkey')
-        // creo el div con clase
     let node = document.createElement("div");
     node.setAttribute("class", "monkey-img");
 
-    // creo la img con los attr necesarios
     let monkeyImg = document.createElement("img");
     monkeyImg.setAttribute("id", "monkey");
     monkeyImg.setAttribute("src", "../assets/monkey-2.png");
 
-    // append child
     node.appendChild(monkeyImg);
     document.getElementById("content").appendChild(node);
 }
 
 function addMagicInput() {
-    console.log('adding input box')
     let node = document.createElement("div");
     node.setAttribute("class", "input-section");
 
     let input = document.createElement("input");
     input.setAttribute("id", "input-box");
+    input.setAttribute("class", "input-box");
+    input.setAttribute("placeholder", "misterious input field");
 
     // append child
     node.appendChild(input);
     document.getElementById("content").appendChild(node);
+}
+
+function bigwords(triggered) {
+    let node = document.createElement("div");
+    node.setAttribute("class", "congrats-img");
+
+    let congrats = document.createElement("img");
+    congrats.setAttribute("id", "congrats-img");
+    congrats.setAttribute("src", "../assets/congrats.png");
+
+    node.appendChild(congrats);
+    if (!triggered) {
+        document.getElementById("content").appendChild(node);
+    }
+    return true;
 }
