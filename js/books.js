8d09757bab4c73fca1b562c80af51680cce5386b
Henry <hobbit.rodrigiu@gmail.com>
2019-12-02 01:43:34 -0300

    add links to book results

--- js/books.js
+++ js/books.js
@@ -21,23 +21,59 @@ function queryBook() {
 
 function parseAndStore(data) {
     const info = JSON.parse(data);
-    // console.log('info', info);
-    populateResults(info.items);
+    searchResults(info.items);
 }
 
-function populateResults(books) {
+function searchResults(books) {
     const section = document.querySelector('.search-results');
-    console.log(books);
-    books.forEach( book => {
+    books.forEach(book => {
+        const link = document.createElement("a");
+        link.setAttribute("href", book.volumeInfo.previewLink);
+        link.setAttribute("alt", book.volumeInfo.title);
+
         const node = document.createElement("div");
         node.setAttribute("class", "book-card");
         if (book.volumeInfo.imageLinks) {
-            let bookImg = document.createElement("img");
-            bookImg.setAttribute("id", book.id);
-            bookImg.setAttribute("src", book.volumeInfo.imageLinks.thumbnail);
-            node.appendChild(bookImg);
-            section.appendChild(node);
+            // poner imagen como background
+            node.style.backgroundImage = `url(${book.volumeInfo.imageLinks.thumbnail})`;
+
+            // poner titulo y descripcion como texto
+            const title = document.createElement("h4");
+            title.setAttribute('class', 'book-card__title');
+            title.textContent = book.volumeInfo.title;
+
+            const description = document.createElement("p");
+            description.setAttribute('class', 'book-card__description');
+            description.textContent = formatedDesc(book.volumeInfo.description);
+
+            node.appendChild(title);
+            node.appendChild(description);
+            link.appendChild(node);
+            section.appendChild(link);
         }
+    })
+
+    addHovers();
+}
+
+function formatedDesc(text) {
+    if (text && text.length > 300) {
+        return `${text.substring(0, 300)}...`;
+    }
+    return text;
+}
+
+function addHovers() {
+    document.querySelectorAll('.book-card').forEach(book => {
+        const image = book.style.backgroundImage;
+        book.addEventListener('mouseover', function() {
+            book.style.backgroundImage = '';
+            book.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+        })
+        book.addEventListener('mouseout', function() {
+            book.style.backgroundImage = image;
+            book.style.backgroundColor = "rgba(0, 0, 0, 0)";
+        })
     })
 }
 
